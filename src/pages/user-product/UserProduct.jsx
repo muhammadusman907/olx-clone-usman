e76331ed8e7ddb7903b11551c881d40cbf021e65
Muhammad usman <122649540+muhammadusman907@users.noreply.github.com>
2024-03-03 22:56:26 +0500

    practice

--- src/pages/user-product/UserProduct.jsx
+++ src/pages/user-product/UserProduct.jsx
@@ -7,17 +7,36 @@ import { AiFillDelete } from "react-icons/ai";
 import { MdEditSquare } from "react-icons/md";
 import { useContext, useEffect, useState } from "react";
 import Auth from "../../context/AuthProvider";
-import { db, collection, query, where, getDocs } from "../../config/firebase";
+import Swal from "sweetalert2";
+
+import {
+  db,
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+  storage,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL
+} from "../../config/firebase";
 import { useForm } from "react-hook-form";
 import {
   MyInput,
   SelectInput,
   UploadInput,
 } from "../../components/input/Input";
+import Loader from "../../components/loader/Loader";
 const UserProduct = () => {
   const { userData } = useContext(Auth);
   const [currentUserProduct, setCurrentUserProduct] = useState([]);
   const [modal2Open, setModal2Open] = useState(false);
+  const [ImageUrl, setImageUrl] = useState();
+  const [loader, setLoader] = useState(false);
+  const [updateProductId, setUpdateProductId] = useState("");
   const {
     control,
     handleSubmit,
@@ -28,6 +47,91 @@ const UserProduct = () => {
   } = useForm({
     defaultValues: {},
   });
+  // update product fuction
+console.log(updateProductId);
+  const uploadImage = (file) => {
+    setLoader(true);
+    return new Promise((resolve, reject) => {
+      // Create the file metadata
+      /** @type {any} */
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+      //Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        () => {
+          // ******************************************************************
+          // <-------- url upload hona ka baad download url mila ga -------->
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUrl(downloadURL);
+            setLoader(false);
+            console.log("File available at", downloadURL);
+          });
+        }
+      );
+    });
+  };
+  const onSubmit = async (data) => {
+    setLoader(true);
+    console.log(ImageUrl);
+    console.log(data);
+    // const docRef = await addDoc(collection(db, "products",), {});
+    const productRef = doc(db, "products",updateProductId);
+    await updateDoc(productRef, {
+      ...data,
+      productImage: ImageUrl,
+      productUserData: { ...userData },
+    });
+    setLoader(false);
+    reset();
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "update SuccessFully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    setModal2Open(false);
+  };
+
+  // *************************************//
+  // <---------- get product -----------> //
+
   const getCurrentUserProduct = async () => {
     let product = [];
     const q = query(
@@ -36,8 +140,7 @@ const UserProduct = () => {
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      //   console.log(doc.id, " => ", doc.data());
-      product.push(doc.data());
+      product.push({ ...doc.data(), userId: doc.id });
     });
     setCurrentUserProduct(product);
   };
@@ -48,16 +151,28 @@ const UserProduct = () => {
   return (
     <>
       <Navbar />
+      {loader && <Loader />}
       <Modal
-        title="Vertically centered modal dialog"
+        maskClosable={false}
+        okButtonProps={{
+          style: {
+            display: "none",
+          },
+        }}
+        cancelButtonProps={{
+          style: {
+            display: "none",
+          },
+        }}
+        title="Update your product"
         centered
         open={modal2Open}
-        // onOk={() => setModal2Open(false)}
-        // onCancel={() => setModal2Open(false)}
+        onOk={() => setModal2Open(false)}
+        onCancel={() => setModal2Open(false)}
       >
         <form
           className="flex flex-col w-full bg-secondary p-5 rounded-md"
-          //   onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <MyInput
             names="title"
@@ -107,11 +222,11 @@ const UserProduct = () => {
               classAdd="mb-2 h-[45px]"
               label="Product Image"
               types="file"
-            //   uploadImageStorage={uploadImage}
+              uploadImageStorage={uploadImage}
               registers={register}
             />
           </div>
-          <Button type="submit" btnName="Submit" classAdd="w-[200px] mt-3" />
+          <Button type="submit" btnName="Update" classAdd="w-[200px] mt-3" />
         </form>
       </Modal>
       <Row>
@@ -143,7 +258,10 @@ const UserProduct = () => {
                 <Button btnIcons={<AiFillDelete />} classAdd="bg-[#df1717]" />
                 <Button
                   btnIcons={<MdEditSquare />}
-                  onClick={() => setModal2Open(true)}
+                  onClick={() => {
+                    setModal2Open(true);
+                    setUpdateProductId(value.userId);
+                  }}
                 />
               </div>
             </div>
